6653d185e0b3c9e25658df7a7040a9b757761e26
“yuxi0203” <zebra12346@hotmail.com>
2016-08-21 21:54:24 -0700

    First checkin, most of them are config.

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    entry: './index.jsx',
+    output: {
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel', // 'babel-loader' is also a legal name to reference
+        }]
+    }
+};
