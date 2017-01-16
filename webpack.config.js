dfabb82ead11de439dc6909b58ff866e7d074949
yuxi0203 <zebra12346@hotmail.com>
2017-01-15 23:03:13 -0800

    Revert "Revert "Fix build problem and add react-router""
    This reverts commit e18fe7f5d72f2744158a5736d27b6b558fa6da88.

--- webpack.config.js
+++ webpack.config.js
@@ -12,5 +12,8 @@ module.exports = {
             exclude: /(node_modules|bower_components)/,
             loader: 'babel', // 'babel-loader' is also a legal name to reference
         }]
+    },
+    devServer: { // Make react-router works
+        historyApiFallback: true
     }
 };
