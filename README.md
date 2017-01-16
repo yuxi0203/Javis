fd9805c4c56c2bd0c1eb203689d48bba4f83575d
Xi Yu <xiyu@Xs-MacBook-Air.local>
2017-01-15 22:55:23 -0800

    Fix build problem and add react-router
    Fix build problem and add react-router

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Javis
 This is a good practise.
+
+After install:
+npm install
+npm install --save-dev babel-preset-react
+npm start
--- index.jsx
+++ index.jsx
@@ -3,10 +3,27 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './components/app';
 import store from './store/store';
+import { Router, Route, Link, browserHistory } from 'react-router'
+
+class About extends React.Component{
+    render() {
+        return (
+            <div>
+                test
+            </div>
+        );
+    }
+}
+
 
 render(
     <Provider store={store}>
-        <App />
+        <Router history={browserHistory}>
+            <Route path="/" component={App}>
+                {/*<IndexRoute component={Home} />*/}
+            </Route>
+            <Route path="about" component={About}/>
+        </Router>
     </Provider>,
     document.getElementById('root')
 )
